f13413bb572d5660408c462f2a42e5577399413c
Henry V. Gilbert <henrygilbert609@gmail.com>
2020-06-02 23:20:27 -0400

    Update and rename CodeSnippets.py to CodeSnippetsCSV.py

--- CodeSnippetsCSV.py
+++ CodeSnippetsCSV.py
@@ -9,22 +9,22 @@ Data = list(Reader)
 # Print Row Data
 row1 = Data[0]
 row2 = Data[1]
-#print("Row 1 is : ", row1)
-#print("Row 2 is : ", row2)
+print("Row 1 is : ", row1)
+print("Row 2 is : ", row2)
 
 # Access a single element.
 toyota_row = Data[1]
 toyota_country = toyota_row[2]
-# print(toyota_country)
-# print(Data[1][2])
+print(toyota_country)
+print(Data[1][2])
 
 
 # Print multiple column indexes
-#print(Data[0][0])
-##print(Data[1][0])
-#print(Data[2][0])
-#print(Data[3][0])
-#print(Data[4][0])
+print(Data[0][0])
+print(Data[1][0])
+print(Data[2][0])
+print(Data[3][0])
+print(Data[4][0])
 
 # Print all columns
 list_of_columns = []
